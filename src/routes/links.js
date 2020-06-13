f7aa33ac65be2bad6ad8aa8e3a685d9e1806836d
Brian <blsvsanteliz@gmail.com>
2020-06-12 23:02:22 -0400

    Deleted to the view and the db

--- src/routes/links.js
+++ src/routes/links.js
@@ -14,7 +14,12 @@ router.post('/add', async (req,res)=>{
 router.get('/', async(req,res)=>{
     const links = await pool.query('SELECT * FROM links')
     res.render('links/list', {links})
-}
-)
+})
+
+router.get('/delete/:id', async (req,res)=>{
+    const { id} = req.params;
+   await pool.query('DELETE FROM links WHERE ID = ?', [id])
+   res.redirect('/links')
+})
 
 module.exports = router;
