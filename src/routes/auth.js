ddfd84c0f976c172e00314f109ca88da07764a56
Brian <blsvsanteliz@gmail.com>
2020-06-13 02:09:46 -0400

    add auth and controllers usign passport

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,12 @@
 const {Router} = require('express')
 const router = Router();
 
+router.get('/registro', (req,res)=>{
+    res.render('auth/registro')
+  
+})
+router.post('/registro', (req,res)=>{
+    res.send('Enviando')
+})
 
 module.exports = router
